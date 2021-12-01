1ea964a13666d19e062e3d70f40533516851537d
Nikroo <curiosityopportunity@gmail.com>
2021-12-01 19:02:05 +0300

    07-spring-xml-spel-external-file-properties

---
+++ src/main/java/by/itacademy/mikhalevich/universe/StartWithXmlPropertiesAutowiring.java
@@ -0,0 +1,20 @@
+package by.itacademy.mikhalevich.universe;
+
+import by.itacademy.mikhalevich.universe.model.StarSystem;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class StartWithXmlPropertiesAutowiring {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext ctx =
+                new ClassPathXmlApplicationContext("beans-with-properties-autowiring.xml");
+        ctx.registerShutdownHook();
+
+        StarSystem solarSystem = ctx.getBean("solarSystem", StarSystem.class);
+        solarSystem.print();
+    }
+
+
+
+}
