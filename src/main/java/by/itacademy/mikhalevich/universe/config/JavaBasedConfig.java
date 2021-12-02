cded76c8007a5abc9a20a1376e6c03b18c0eaa40
Nikroo <curiosityopportunity@gmail.com>
2021-12-02 19:53:38 +0300

    16-spring-annotations-java-based-properties

--- src/main/java/by/itacademy/mikhalevich/universe/config/JavaBasedConfig.java
+++ src/main/java/by/itacademy/mikhalevich/universe/config/JavaBasedConfig.java
@@ -3,24 +3,34 @@ package by.itacademy.mikhalevich.universe.config;
 import by.itacademy.mikhalevich.universe.model.system.LifePlanet;
 import by.itacademy.mikhalevich.universe.model.system.NonLifePlanet;
 import by.itacademy.mikhalevich.universe.model.system.StarSystem;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@PropertySource({"planet.properties"})
 public class JavaBasedConfig {
+
+    @Value("${earth.id}")
+    private Integer earthId;
+    @Value("${earth.name}")
+    private String earthName;
+    @Value("#{${earth.lifeForm}}")
+    private Map<String, Integer> earthLifeForm;
 
     @Bean
     public LifePlanet earth(){
-        LifePlanet earth = new LifePlanet(1, "Earth");
-        earth.setLifeForm(Map.of("Human", 100, "Plants", 200, "Bactery", 300));
+        LifePlanet earth = new LifePlanet(earthId, earthName);
+        earth.setLifeForm(earthLifeForm);
         return earth;
     }
 
     @Bean
-    public NonLifePlanet mars(){
-        NonLifePlanet mars = new NonLifePlanet(2, "Mars", true);
+    public NonLifePlanet mars(@Value("${mars.id}") int id,  @Value("${mars.name}") String name, @Value("${mars.isColonization}") boolean isColonization){
+        NonLifePlanet mars = new NonLifePlanet(id, name, isColonization);
         return mars;
     }
 
