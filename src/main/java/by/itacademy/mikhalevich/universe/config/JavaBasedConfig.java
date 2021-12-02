a2449ba3608244aac8e1bcb10b8ed7cb365c5a0a
Nikroo <curiosityopportunity@gmail.com>
2021-12-02 19:24:08 +0300

    15-spring-annotations-java-based-beans

--- src/main/java/by/itacademy/mikhalevich/universe/config/JavaBasedConfig.java
+++ src/main/java/by/itacademy/mikhalevich/universe/config/JavaBasedConfig.java
@@ -5,32 +5,44 @@ import by.itacademy.mikhalevich.universe.model.system.NonLifePlanet;
 import by.itacademy.mikhalevich.universe.model.system.StarSystem;
 import org.springframework.context.annotation.Bean;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class JavaBasedConfig {
 
+    @Bean
     public LifePlanet earth(){
         LifePlanet earth = new LifePlanet(1, "Earth");
         earth.setLifeForm(Map.of("Human", 100, "Plants", 200, "Bactery", 300));
         return earth;
     }
 
+    @Bean
     public NonLifePlanet mars(){
         NonLifePlanet mars = new NonLifePlanet(2, "Mars", true);
         return mars;
     }
 
+    @Bean
     public NonLifePlanet venus(){
         NonLifePlanet venus = new NonLifePlanet(3, "Venus", false);
         return venus;
     }
 
     @Bean
-    public StarSystem solarSystem(){
+    public List<NonLifePlanet> nonLifePlanets(NonLifePlanet mars, NonLifePlanet venus){
+        List<NonLifePlanet> nonLifePlanetsList = new ArrayList<>();
+        nonLifePlanetsList.add(mars);
+        nonLifePlanetsList.add(venus);
+        return nonLifePlanetsList;
+    }
+
+    @Bean
+    public StarSystem solarSystem(LifePlanet earth, List<NonLifePlanet> nonLifePlanets){
         StarSystem solarSystem = new StarSystem(4, "Solar System", "SLRSSTM");
-        solarSystem.setLifePlanet(earth());
-        solarSystem.setNonLifePlanets(List.of(mars(), venus()));
+        solarSystem.setLifePlanet(earth);
+        solarSystem.setNonLifePlanets(nonLifePlanets);
         return solarSystem;
     }
 
